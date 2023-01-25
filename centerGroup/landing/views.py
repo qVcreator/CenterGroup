4165ad2ca2e22fc9016f511d31e61b2467d82490
Timur <2001rich04@gmail.com>
2023-01-26 02:02:32 +0300

    refactor views.py

--- centerGroup/landing/views.py
+++ centerGroup/landing/views.py
@@ -28,6 +28,34 @@ class MainPageView(TemplateView):
 
         return self.render_to_response(context)
 
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
+
+        comment_form = CommentForm(request.POST)
+        feedback_form = FeedbackForm(request.POST)
+
+        context['feedback_form'] = feedback_form
+        context['comment_form'] = comment_form
+
+        if 'comm_btn' in request.POST:
+            if comment_form.is_valid():
+                comment_form.save()
+                return self.render_to_response(context)
+            else:
+                context['message'] = "Проверьте введенные данные"
+                return self.render_to_response(context)
+
+        if 'cons_btn' in request.POST:
+            if feedback_form.is_valid():
+                # feedback_form.save()
+
+                send_email(feedback_form)
+
+                return self.render_to_response(context)
+            else:
+                context['message'] = "Проверьте введенные данные"
+                return self.render_to_response(context)
+
 
 def apartment_page(request):
     context = {
@@ -36,54 +64,11 @@ def apartment_page(request):
     return render(request, 'landing/apartment.html', context=context)
 
 
-class FeedbackFormView(FormView):
-    form_class = FeedbackForm
-    template_name = 'landing/home.html'
-    success_url = reverse_lazy('home')
+def send_email(form: FeedbackForm):
+    user_name = form.cleaned_data.get("Name")
+    user_phone = form.cleaned_data.get("PhoneNumber")
+    user_mail = form.cleaned_data.get("Email")
 
-    def post(self, request, *args, **kwargs):
-        feedback_form = self.form_class(request.POST)
-        if feedback_form.is_valid():
-            feedback_form.save()
-
-            user_name = feedback_form.cleaned_data.get("Name")
-            user_phone = feedback_form.cleaned_data.get("PhoneNumber")
-            user_mail = feedback_form.cleaned_data.get("Email")
-
-            body = f'Добрый день, меня зовут {user_name}!\nПрошу связаться со мной по почте ({user_mail}) или по телефону {user_phone} '
-            email = EmailMessage(f'Консультация-{user_name}', body, to=[os.environ['EMAIL_TO']])
-            email.send()
-            
-            return self.render_to_response(
-                self.get_context_data(
-                    message="Ваш запрос отправлен"
-                )
-            )
-        else:
-            return self.render_to_response(
-                self.get_context_data(
-                    message="Проверьте введенные данные"
-                )
-            )
-
-
-class CommentFormView(FormView):
-    form_class = CommentForm
-    template_name = 'landing/home.html'
-    success_url = reverse_lazy('home')
-
-    def post(self, request, *args, **kwargs):
-        comment_form = self.form_class(request.POST)
-        if comment_form.is_valid():
-            comment_form.save()
-            return self.render_to_response(
-                self.get_context_data(
-                    success=True
-                )
-            )
-        else:
-            return self.render_to_response(
-                self.get_context_data(
-                    message="Проверьте введенные данные"
-                )
-            )
+    body = f'Добрый день, меня зовут {user_name}!\nПрошу связаться со мной по почте ({user_mail}) или по телефону {user_phone} '
+    email = EmailMessage(f'Консультация-{user_name}', body, to=[os.environ['EMAIL_TO']])
+    email.send()
